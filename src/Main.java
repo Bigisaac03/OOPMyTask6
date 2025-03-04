0f1cde04b6d921691bbfb7147772e040f9188b4f
Bigisaac03 <amanicibembe7@gmail.com>
2025-03-04 14:14:00 +0300

    commit #2
    in the nv, on line 1-2, we imported the two packages from diffrents modules, on line 5-6, we created an instance of a class and call the method, from line 8-10, we created an instance of the class and called two methods from our class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-
+import Package1.UserDetails;
+import Package2.UserItems;
 public class Main {
     public static void main(String[] args) {
-        
+        UserDetails User=new UserDetails("Big isaac",55677,2003);
+        User.displayUserDetails();
+
+        UserItems itm= new UserItems("RoyceRoss","Honda","jordan1",07);
+        UserItems.AddMyItems(itm);
+        itm.displayitems();
+
     }
 }
