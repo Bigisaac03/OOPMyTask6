ce17ef7e39336f0a3de6723b50829fead4761de4
Bigisaac03 <amanicibembe7@gmail.com>
2025-03-04 11:54:51 +0300

    commit #3
    in the nv, from line 5-7, we declared variables, on line 9, we created a constructor, from line 10-12, we initialized the variables, from line 14-22, we created 3 getmethods for returning our variables, on line 23, we created a method to for displaying our varibles, from line 24-26, we displayed the variables

--- Myfirstmodule/src/Package1/UserDetails.java
+++ Myfirstmodule/src/Package1/UserDetails.java
@@ -1,4 +1,28 @@
 package Package1;
 
 public class UserDetails {
+    //declaring variables
+    String UserName;
+    int ID;
+    int password;
+    //constructor
+    public UserDetails(String UserName, int ID, int password){
+        this.UserName=UserName;
+        this.ID=ID;
+        this.password=password;
+    }
+    String GetUserName(){
+        return UserName;
+    }
+    int GetID(){
+        return ID;
+    }
+    int Getpassword(){
+        return password;
+    }
+    public void displayUserDetails(){
+        System.out.println("User name is "+UserName);
+        System.out.println("User ID is "+ID);
+        System.out.println("User password is "+password);
+    }
 }
